11d3f2aa7a8f521d047458c127b2cc707b854bc7
HallisCode <applgoolhal@gmail.com>
2023-01-28 13:29:58 +0300

    Add project files.

---
+++ XorCipher/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+
+namespace XorCipher
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            while (true)
+            {
+                DisplayMenu();
+            }
+        }
+
+        private static void DisplayMenu()
+        {
+            int commandNumber = 0;
+
+            Console.WriteLine(
+                    "List of commands:\n\n" +
+                    "> 1 Encryption\n" +
+                    "> 2 Decryption\n" +
+                    "> 3 Exit \n\n" +
+                    "Enter the number of command :");
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out commandNumber)
+                    && commandNumber > 0 && commandNumber <= 3)
+                {
+                    break;
+                };
+
+                Console.WriteLine("\nThis command does not exist, enter the command :");
+            }
+
+            switch (commandNumber)
+            {
+                case (1):
+                    Encryption();
+                    break;
+
+                case (2):
+                    Decryption();
+                    break;
+
+                case (3):
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static void Encryption()
+        {
+            string message = "";
+            string key = "";
+
+            string messagePath = InputFilePath("Enter the path to the text file containing your message :");
+            string pathResult = InputDirectoryPath("Enter the path to the folder where the encrypted text and the key will be located:");
+
+            using (StreamReader fileMessage = new StreamReader(messagePath))
+            {
+                message = fileMessage.ReadToEnd();
+            }
+
+            string result = XorCipher.Encryption(message, out key);
+
+            using (StreamWriter fileMessage = new StreamWriter(pathResult + @"\message.txt", false, System.Text.Encoding.UTF8))
+            {
+                fileMessage.Write(result);
+            }
+
+            using (StreamWriter fileKey = new StreamWriter(pathResult + @"\key.txt", false, System.Text.Encoding.UTF8))
+            {
+                fileKey.Write(key);
+            }
+
+            Console.WriteLine("\nCompleted!\n");
+        }
+
+        private static void Decryption()
+        {
+            string message = "";
+            string key = "";
+
+            string messagePath = InputFilePath("Enter the path to the text file containing the encrypted message :");
+            string keyPath = InputFilePath("Enter the path to the text file, with the key :");
+
+            using (StreamReader fileMessage = new StreamReader(messagePath))
+            {
+                message = fileMessage.ReadToEnd();
+            }
+
+            using (StreamReader fileKey = new StreamReader(keyPath))
+            {
+                key = fileKey.ReadToEnd();
+            }
+
+            string result = XorCipher.Decryption(message, key);
+
+            Console.WriteLine($"\nDecrypted Message :\n{result}\n\n");
+        }
+
+        private static string InputFilePath(string message)
+        {
+            Console.WriteLine($"\n{message}");
+            string path = Console.ReadLine();
+
+            while (!File.Exists(path))
+            {
+                Console.WriteLine("\nIncorrect path to the file is specified, enter it again:");
+                path = Console.ReadLine();
+            }
+
+            return path;
+        }
+
+        private static string InputDirectoryPath(string message)
+        {
+            Console.WriteLine($"\n{message}");
+            string path = Console.ReadLine();
+
+            while (!Directory.Exists(path))
+            {
+                Console.WriteLine("\nIncorrect directory path specified, enter it again:");
+                path = Console.ReadLine();
+            }
+
+            return path;
+        }
+    }
+}
---
+++ XorCipher/XorCipher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace XorCipher
+{
+    internal class XorCipher
+    {
+        private static Random _random = new Random();
+
+
+        public static string Encryption(string message, out string key)
+        {
+            string encryptedMessage = "";
+            key = "";
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Char symbol = message[i];
+
+                if (symbol == ' ')
+                {
+                    encryptedMessage += ' ';
+                    key += ' ';
+                    continue;
+                }
+
+                UInt16 symbolCode = Convert.ToUInt16(symbol);
+                UInt16 keySymbolCode = (UInt16)_random.Next(0, UInt16.MaxValue);
+
+                char encryptedSymbol = (char)(symbolCode ^ keySymbolCode);
+
+                key += (char)keySymbolCode;
+                encryptedMessage += encryptedSymbol;
+            }
+
+            return encryptedMessage;
+        }
+
+        public static string Decryption(string encryptedMessage, string key)
+        {
+            string message = "";
+
+            if (encryptedMessage.Length != key.Length)
+            {
+                return message;
+            }
+
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                if (encryptedMessage[i] == ' ')
+                {
+                    message += ' ';
+                    continue;
+                }
+
+                UInt16 symbolCode = Convert.ToUInt16(encryptedMessage[i]);
+                UInt16 keySymbolCode = Convert.ToUInt16(key[i]);
+
+                char messageSymbol = (char)(symbolCode ^ keySymbolCode);
+                message += messageSymbol;
+            }
+
+            return message;
+        }
+    }
+}
